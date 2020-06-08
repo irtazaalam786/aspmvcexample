0b95a743629879805adbc2cd70f3baba4d95f0fd
Irtaza Alam <irtazaalam786@gmail.com>
2020-06-08 15:29:47 +0500

    entity framework set

---
+++ ASPMVCCRUD/ASPMVCCRUD/Models/ASPContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ASPMVCCRUD.Models;
+
+namespace ASPMVCCRUD.Models
+{
+    public class ASPContext : DbContext
+    {
+        //Entities
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
